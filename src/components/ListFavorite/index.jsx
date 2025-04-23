eff2acb4bb9f95ca528fed67422b230f0113a423
ArianeDev <silvaarianeoliveira2@gmail.com>
2025-04-23 11:29:33 -0300

    Updates

--- src/components/ListFavorite/index.jsx
+++ src/components/ListFavorite/index.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
+import response from '../../services/api';
 
 export function ListFavorite() {
     const [listFavorite, setlistFavorite] = useState(null);
     
     useEffect(() => {
         const loadAll = async () => {
-            // Pegando a lista toda
             let list = await response.get_home();
+            const listMovie = []
 
-            let randomChosen = Math.floor(Math.random() * (list[0].items.results.length - 1));
-            let chosen = originals[0].items.results[randomChosen];
-            console.log(chosen)
-            setlistFavorite(chosen)
+            for(let i = 0; i < 3; i++){
+                let randomChosen = Math.floor(Math.random() * (list[0].items.results.length - 1));
+                let chosen = list[0].items.results[randomChosen];
+
+                listMovie.push(chosen)
+            }
+            
+            setlistFavorite(listMovie);
+            console.log(listMovie)
         }
 
         loadAll();
@@ -19,6 +25,7 @@ export function ListFavorite() {
 
     return(
         <>
+            {listFavorite}
         </>
     )
 }
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,7 +1,9 @@
+import { ListFavorite } from '../../components/ListFavorite';
+
 export function User() {
     return (
         <div className="page">
-            
+            <ListFavorite />
         </div>
     );
 }
